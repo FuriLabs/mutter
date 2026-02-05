88f085958c49ec3257d7a6a718daf73d07b1972b
Bardia Moshiri <bardia@furilabs.com>
2026-02-05 09:31:41 -0500

    wayland: Implement virtual_keyboard_unstable_v1 wayland protocol
    implement virtual_keyboard_unstable_v1 (https://wayland.app/protocols/virtual-keyboard-unstable-v1) which allows us to send virtual keyboard events from external processes.
    this commit also makes wtype work.
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

---
+++ src/wayland/meta-wayland-virtual-keyboard.h
@@ -0,0 +1,31 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright 2026 Furi Labs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Bardia Moshiri <bardia@furilabs.com>
+ */
+
+#pragma once
+
+#include "wayland/meta-wayland-types.h"
+
+#define META_TYPE_WAYLAND_VIRTUAL_KEYBOOARD (meta_wayland_virtual_keyboard_resource_get_type ())
+G_DECLARE_FINAL_TYPE (MetaWaylandVirtualKeyboard,
+                      meta_wayland_virtual_keyboard_resource,
+                      META, WAYLAND_VIRTUAL_KEYBOARD,
+                      GObject);
+
+void meta_wayland_virtual_keyboard_init (MetaWaylandCompositor *compositor);
